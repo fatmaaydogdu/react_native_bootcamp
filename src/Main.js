a45fdef038cd95f17858fce3732187bbf86791e5
Fatma Aydoğdu <fatmaaydogdu581@gmail.com>
2021-01-01 21:33:38 +0300

    flatlist component initialized

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { SafeAreaView,View, } from 'react-native';
+import { FlatList, SafeAreaView,View,Text} from 'react-native';
 import Input from './component/Input';
+
+const myArray = [
+    {
+        id:0,
+        name: 'Fatma Fatma Fatma Fatma Fatma Fatma Fatma Fatma Fatma Fatma Fatma',
+        age:19,
+        isAdmin:true,
+    },
+    {
+        id:1,
+        name:'Turan Turan Turan Turan Turan Turan Turan Turan Turan Turan Turan',
+        age:15,
+        isAdmin:false,
+    }, {
+        id:2,
+        name:'Emine Emine Emine Emine Emine Emine Emine Emine Emine Emine Emine',
+        age:40,
+        isAdmin:true,
+    },
+]
 
 function Main(){
     return(
         <SafeAreaView style={{flex:1}}>
             <View style={{flex:1}}>
-                <Input 
-                label="E-posta: "
-                placeholder="E-posta adresinizi giriniz..."
-                onText={(name) => console.log(name)}
-                type="email-address"
-                />
+                
+                <FlatList
+                data={myArray}
+                renderItem={({item}) => <Text>{item.name}</Text>}
 
-                <Input
-                label="Şifreniz: "
-                placeholder="Şifrenizi giriniz..."
-                onText={(surname) => console.log(surname)}
                 />
                 </View>  
         </SafeAreaView>
