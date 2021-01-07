b4cec1f7b38408cbb2e94f3b9fd22e5b635c7590
Fatma Aydoğdu <fatmaaydogdu581@gmail.com>
2021-01-08 01:12:41 +0300

    State

--- src/Main.js
+++ src/Main.js
@@ -1,45 +1,18 @@
-import React from 'react';
-import { FlatList, SafeAreaView,View,Text} from 'react-native';
-import Input from './component/Input';
-import UserItem from './component/UserItem';
+import React, {useState} from 'react';
+import { FlatList, SafeAreaView,View,Text,Button} from 'react-native';
 
-const myArray = [
-    {
-        id:0,
-        name: 'There was something special about this little creature.',
-        age:19,
-        isAdmin:true,
-    },
-    {
-        id:1,
-        name:'Donna couldnt quite pinpoint what it was, but she knew with all her heart that it was true. ',
-        age:15,
-        isAdmin:false,
-    }, {
-        id:2,
-        name:'It wasnt a matter of if she was going to try and save it, but a matter of how she was going to save it.',
-        age:40,
-        isAdmin:true,
-    },
-]
-
-
-
-
-    
 
 function Main(){
-
-       const Renderuser=({item}) => <UserItem user={item}/> 
+const[counter,setCounter] = useState(0);
 
     return(
     <SafeAreaView style={{flex: 1}}>
         <View style={{flex: 1}}>
-            <FlatList
-            data={myArray}
-            keyExtractor={(item,index) => index.toString()}
-            renderItem={Renderuser} />
-
+            <Text style={{fontSize:40}}>Counter: {counter}</Text>
+            <Button title= 'up' 
+            onPress={() => setCounter(counter + 1)}
+            />
+            
         </View>
     </SafeAreaView>
     )
